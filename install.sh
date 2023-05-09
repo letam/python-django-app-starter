738db2f417e5a1cf236843ac476aecbaa986a118
Tam Le <tamsgit@gmail.com>
2023-05-08 21:18:47 -0400

    install.sh: Add support for args in Bash scipt
    - Handle arg for --dev to install dev dependencies in Python script

--- install.sh
+++ install.sh
@@ -3,6 +3,13 @@
 # Directory where the virtual environment is located.
 venv_dir=".venv"
 
+for arg in "$@"; do
+	case "$arg" in
+		--dev) # Install dev dependencies
+			install_dev_dependencies=1
+	esac
+done
+
 # Ensure the virtual environment exists.
 if [ ! -d "$venv_dir" ]; then
 	./install/create_venv.py
@@ -10,4 +17,8 @@ fi
 
 # Install project dependencies.
 echo "Installing project dependencies..."
-./install/install_dependencies.py
+if [ ! -z $install_dev_dependencies ]; then
+	./install/install_dependencies.py --dev
+else
+	./install/install_dependencies.py
+fi
